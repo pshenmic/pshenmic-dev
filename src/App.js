215132d55801696408747e04db3dce224007673e
pshenmic <pshenmic@gmail.com>
2023-10-15 15:33:34 +0700

    Basic info

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        pshenmic's development page
+          <span>pshenmic's development page</span>
+
+          <div className={"github"}>
+             <img src={"assets/img.png"}/>
+              <div>open source blockchain developer</div>
+             <a href={"https://github.com/pshenmic"}>github.com/pshenmic</a>
+          </div>
       </header>
     </div>
   );
