4685a1e0d9f496e9a8786f6dc59b49c97f8ffe75
Alexey <andreewsky.alexey@gmail.com>
2024-08-05 19:54:44 +0300

    Setting up linter and basic code style refactoring

--- src/components/Team/Teammate.js
+++ src/components/Team/Teammate.js
@@ -1,65 +1,59 @@
-import './Teammate.scss'
 import Link from 'next/link'
 import { motion as m } from 'framer-motion'
+import './Teammate.scss'
 
+function Teammate ({ name, username, role, photoSrc, discord, github, twitter, id }) {
+  return (
+    <m.div
+      className={'Teammate'}
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      whileHover={{
+        x: -5,
+        y: -5,
+        transition: { duration: 0.5 }
+      }}
+      transition={{ duration: 0.5, delay: id / 10 }}
+    >
+      <div className={'Teammate__ImageContainer'}>
+        <img src={photoSrc}/>
+      </div>
 
-function Teammate ({name, username, role, photoSrc, discord, github, twitter, id}) {
-    return (
-        <m.div 
-            className="Teammate"
-            initial={{ y: 30, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            whileHover={{
-                x: -5,
-                y: -5,
-                transition: { duration: .5 },
-            }}
-            transition={{ duration: .5, delay: id/10 }}
-        >
+      <div className={'Teammate__ContentContainer'}>
+        <div className={'Teammate__Title'}>
+          <div className={'Teammate__Name'}>{name}</div>
+          <div className={'Teammate__Username'}>@{username}</div>
+        </div>
 
-            <div className="Teammate__ImageContainer">
-                <img src={ photoSrc }/>
-            </div>
+        <div className={'Teammate__Description'}>{role}</div>
 
-            <div className='Teammate__ContentContainer'>
+        <div className={'Teammate__Socials'}>
+          {(github &&
+            <Link
+              href={github}
+              target={'_blank'}
+              className={'Teammate__SocialLink Teammate__SocialLink--Github'}>
+            </Link>
+          )}
 
-                <div className="Teammate__Title">
-                    <div className="Teammate__Name">{ name }</div>
-                    <div className="Teammate__Username">@{ username }</div>
-                </div>
+          {(discord &&
+            <Link
+              href={discord}
+              target={'_blank'}
+              className={'Teammate__SocialLink Teammate__SocialLink--Discord'}>
+            </Link>
+          )}
 
-                <div className="Teammate__Description">{ role }</div>
-
-                <div className='Teammate__Socials'>
-
-                    {(github &&
-                        <Link 
-                            href={github} 
-                            target="_blank" 
-                            className="Teammate__SocialLink Teammate__SocialLink--Github">
-                        </Link>
-                    )}
-
-                    {(discord &&
-                        <Link 
-                            href={discord} 
-                            target="_blank" 
-                            className="Teammate__SocialLink Teammate__SocialLink--Discord">
-                        </Link>
-                    )}
-
-                    {(twitter &&
-                        <Link 
-                            href={twitter} 
-                            className="Teammate__SocialLink Teammate__SocialLink--Twitter">
-                        </Link>
-                    )}
-
-                </div>
-
-            </div>
-        </m.div>
-    )
+          {(twitter &&
+            <Link
+              href={twitter}
+              className={'Teammate__SocialLink Teammate__SocialLink--Twitter'}>
+            </Link>
+          )}
+        </div>
+      </div>
+    </m.div>
+  )
 }
 
 export default Teammate
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -2,71 +2,64 @@ import Teammate from './Teammate'
 import { motion as m } from 'framer-motion'
 import './Team.scss'
 
-
 function Team () {
-    return (
-        <m.div
-            className="Team"
-        >
+  return (
+    <m.div className={'Team'}>
+      <div className={'Team__TeammateList'}>
+        <Teammate
+          id={1}
+          name={'Mikhail'}
+          username={'pshenmic'}
+          role={'Lead / Coder'}
+          gpg={'A0CE DD79 11C6 3ACB 60D3  8648 9468 3166 E328 0A61'}
+          photoSrc={'https://avatars.githubusercontent.com/u/17009187?v=4'}
+          github={'https://github.com/pshenmic'}
+          twitter={'https://twitter.com/pshenmic'}
+          discord={'https://discordapp.com/users/784071440864378911'}
+        />
 
-            <div className="Team__TeammateList">
+        <Teammate
+          id={2}
+          name={'Alex'}
+          username={'alexeyandreevsky'}
+          role={'Frontend dev'}
+          photoSrc={'https://avatars.githubusercontent.com/u/143793672?v=4'}
+          github={'https://github.com/alexeyandreevsky'}
+          discord={'https://discordapp.com/users/390865069195657216'}
+        />
 
-                <Teammate
-                    id={1}
-                    name={'Mikhail'}
-                    username={'pshenmic'}
-                    role={'Lead / Coder'}
-                    gpg={"A0CE DD79 11C6 3ACB 60D3  8648 9468 3166 E328 0A61"}
-                    photoSrc={'https://avatars.githubusercontent.com/u/17009187?v=4'}
-                    github={'https://github.com/pshenmic'}
-                    twitter={'https://twitter.com/pshenmic'}
-                    discord={'https://discordapp.com/users/784071440864378911'}
-                />
+        <Teammate
+          id={2}
+          name={'Owl352'}
+          username={'owl352'}
+          role={'Designer / Frontend dev'}
+          photoSrc={'https://avatars.githubusercontent.com/u/64574305?v=4'}
+          github={'https://github.com/alexeyandreevsky'}
+          discord={'owl352'}
+        />
 
-                <Teammate
-                    id={2}
-                    name={'Alex'}
-                    username={'alexeyandreevsky'}
-                    role={'Frontend dev'}
-                    photoSrc={'https://avatars.githubusercontent.com/u/143793672?v=4'}
-                    github={'https://github.com/alexeyandreevsky'}
-                    discord={'https://discordapp.com/users/390865069195657216'}
-                />
+        <Teammate
+          id={2}
+          name={'Designer'}
+          username={'designer'}
+          role={'UX designer'}
+          photoSrc={'https://avatars.githubusercontent.com/u/91994843?v=4'}
+          github={'https://github.com/blackmirrordesigner'}
+          discord={'blackmirrordesigner'}
+        />
 
-                <Teammate
-                    id={2}
-                    name={'Owl352'}
-                    username={'owl352'}
-                    role={'Designer / Frontend dev'}
-                    photoSrc={'https://avatars.githubusercontent.com/u/64574305?v=4'}
-                    github={'https://github.com/alexeyandreevsky'}
-                    discord={'owl352'}
-                />
-
-                <Teammate
-                    id={2}
-                    name={'Designer'}
-                    username={'designer'}
-                    role={'UX designer'}
-                    photoSrc={'https://avatars.githubusercontent.com/u/91994843?v=4'}
-                    github={'https://github.com/blackmirrordesigner'}
-                    discord={'blackmirrordesigner'}
-                />
-
-                <Teammate
-                    id={2}
-                    name={'Den Kravchu'}
-                    username={'designer'}
-                    role={'Frontend dev'}
-                    photoSrc={'https://avatars.githubusercontent.com/u/58281610?v=4'}
-                    github={'https://github.com/denkravchu'}
-                    discord={'blackmirrordesigner'}
-                />
-
-            </div>
-
-        </m.div>
-    )
+        <Teammate
+          id={2}
+          name={'Den Kravchu'}
+          username={'designer'}
+          role={'Frontend dev'}
+          photoSrc={'https://avatars.githubusercontent.com/u/58281610?v=4'}
+          github={'https://github.com/denkravchu'}
+          discord={'blackmirrordesigner'}
+        />
+      </div>
+    </m.div>
+  )
 }
 
 export default Team
