9d6764a0760d33df2df5b67a72fe5881cf8d03fb
Den Kravchu <denkravchu@gmail.com>
2024-08-29 19:28:25 +0300

    Add background modals

---
+++ src/components/UI/DarkWrapper/DarkWrapper.jsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { easings, useSpring, animated } from "@react-spring/web";
+import "./DarkWrapper.scss";
+
+function DarkWrapper ({ duration = 600, open, children }) {
+    const animationPopUpWrapper = useSpring({
+        to: {
+            opacity: open ? '1' : '0',
+            pointerEvents: open ? 'all' : 'none'
+        },
+        config: {
+            duration: duration,
+            easing: easings.easeOutQuad
+        }
+    })
+
+    return(
+        <animated.div className={'DarkWrapper'} style={animationPopUpWrapper}>
+            {children}
+        </animated.div>
+    )
+}
+
+export default DarkWrapper
