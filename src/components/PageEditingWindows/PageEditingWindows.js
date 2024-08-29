3a6ec54a9c9955e597e02b514157d723632c34fb
Den Kravchu <denkravchu@gmail.com>
2024-08-29 19:26:48 +0300

    Add edit forms

---
+++ src/components/PageEditingWindows/PageEditingWindows.js
@@ -0,0 +1,23 @@
+'use client'
+
+import TeamEditingWindow from './TeamEditingWindow'
+import UpdatesEditingWindow from './UpdatesEditingWindow'
+import ServicesEditingWindow from './ServicesEditingWindow'
+import ProjectEditingWindow from './ProjectEditingWindow'
+import { usePathname } from 'next/navigation'
+import './PageEditingWindows.scss'
+
+function PageEditingWindows ({ clearErrors, errors, register, setValue }) {
+    const path = usePathname()
+
+    return (
+        <>
+          { path === '/admin' && <UpdatesEditingWindow key={'UpdatesEditingWindow'} errors={errors} register={register} setValue={setValue} clearErrors={clearErrors}/> }
+          { path === '/admin/services' && <ServicesEditingWindow key={'ServicesEditingWindow'} errors={errors} register={register} setValue={setValue} clearErrors={clearErrors}/> }
+          { path === '/admin/projects' &&<ProjectEditingWindow key={'ProjectEditingWindow'} errors={errors} register={register} setValue={setValue} clearErrors={clearErrors}/> }
+          { path === '/admin/team' && <TeamEditingWindow key={'TeamEditingWindow'} errors={errors} register={register} setValue={setValue} clearErrors={clearErrors}/> }
+        </>
+    )
+}
+
+export default PageEditingWindows
