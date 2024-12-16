ada21dca74e7efc31f7702bf9980a8439eb3bba7
Den Kravchu <denkravchu@gmail.com>
2024-12-16 16:26:21 +0300

    Updated dash

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,13 @@ import EditingWindow from '@/components/UI/EditingWindow/EditingWindow'
 import RegistrationWindow from '@/components/Registration/RegistrationWindow'
 import ImportWalletWindow from '@/components/Registration/ImportWalletWindow'
 import dynamic from "next/dynamic"
+import { DashProvider } from '@/hooks/useDashClient'
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.css';
-import { DashProvider } from '@/hooks/useDashClient'
 
 const ToastContainer = dynamic(() =>
-    import('react-toastify').then((mod) => mod.ToastContainer), 
-    { ssr: false }
+  import('react-toastify').then((mod) => mod.ToastContainer),
+  { ssr: false }
 )
 
 export const metadata = {
@@ -17,19 +17,19 @@ export const metadata = {
   description: 'Developer space of pshenmic'
 }
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
-    <DashProvider>
-      <html lang={'en'}>
-        <body>
+    <html lang={'en'}>
+      <body>
+        <DashProvider>
           <ToastContainer />
           <HasAdminAccess />
           <EditingWindow />
           <RegistrationWindow />
-        <ImportWalletWindow />
-        {children}
+          <ImportWalletWindow />
+          {children}
+        </DashProvider>
       </body>
     </html>
-    </DashProvider>
   )
 }
