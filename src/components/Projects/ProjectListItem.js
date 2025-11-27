149fd1dc37b19669f85edab42bb7d4be242707e6
Alexey <andreewsky.alexey@gmail.com>
2025-11-27 22:51:07 +0300

    Update projects list

--- src/components/Projects/ProjectListItem.js
+++ src/components/Projects/ProjectListItem.js
@@ -26,14 +26,14 @@ function ProjectListItem({ project, openHandler, id, openEditor }) {
       transition={{ duration: 0.5, delay: id / 10 }}
     >
       {/* <p className={'ProjectListItem__PendingClaim'}>PENDING CLAIM</p> */}
-      <p className={`ProjectListItem__Domain ${admin ? 'ProjectListItem__Domain--Admin' : ''}`}>
-        {project?.ownerName ? formatOwnerId(project.ownerName) : formatOwnerId(project.ownerId) || ''}
-      </p>
-      {admin
-        ? <div className={'ProjectListItem__WrapperEditButton'} onClick={(e) => e.stopPropagation()}>
-          <EditButton handleClick={() => openEditor(project)} type={'buttom'} />
-        </div>
-        : null}
+      {/*<p className={`ProjectListItem__Domain ${admin ? 'ProjectListItem__Domain--Admin' : ''}`}>*/}
+      {/*  {project?.ownerName ? formatOwnerId(project.ownerName) : formatOwnerId(project.ownerId) || ''}*/}
+      {/*</p>*/}
+      {/*{admin*/}
+      {/*  ? <div className={'ProjectListItem__WrapperEditButton'} onClick={(e) => e.stopPropagation()}>*/}
+      {/*    <EditButton handleClick={() => openEditor(project)} type={'buttom'} />*/}
+      {/*  </div>*/}
+      {/*  : null}*/}
       <div className={'ProjectListItem__ImageContainer'}>
         <img
           alt={project.title || ''}
@@ -45,33 +45,33 @@ function ProjectListItem({ project, openHandler, id, openEditor }) {
       <div className={'ProjectListItem__ContentContainer'}>
         <p className={'ProjectListItem__Title'}>{project.name}</p>
         <ReactMarkdown remarkPlugins={[remarkGfm]} className={'ProjectListItem__Description'}>{truncateText(project.description, 75)}</ReactMarkdown>
-        <div className={'ProjectListItem__WrapperButtons'}>
-          <RegistrationButton
-            className={'ProjectListItem__ButtonTasks'}
-            text={project?.tasts?.length > 0 ? `${project.tasts.length} Tasks` : 'No Tasks'}
-            ariaLabel={'Tasks'}
-            disabled={project?.tasts?.length === 0}
-          />
-          {project?.pendingClaims?.length > 0 ?
-            <RegistrationButton
-              className={'ProjectListItem__ButtonPendingClaims'}
-              text={`${project.pendingClaims.length} Pending Claims`}
-              ariaLabel={'Pending Claims'}
-            />
-            : null
-          }
-        </div>
-        <div className={'ProjectListItem__Footer'}>
-          <Grade />
-          {admin ?
-            <RegistrationButton
-              className={'ProjectListItem__ButtonAddTask'}
-              text={`ADD TASK`}
-              ariaLabel={'ADD TASK'}
-            />
-            : null
-          }
-        </div>
+        {/*<div className={'ProjectListItem__WrapperButtons'}>*/}
+        {/*  <RegistrationButton*/}
+        {/*    className={'ProjectListItem__ButtonTasks'}*/}
+        {/*    text={project?.tasts?.length > 0 ? `${project.tasts.length} Tasks` : 'No Tasks'}*/}
+        {/*    ariaLabel={'Tasks'}*/}
+        {/*    disabled={project?.tasts?.length === 0}*/}
+        {/*  />*/}
+        {/*  {project?.pendingClaims?.length > 0 ?*/}
+        {/*    <RegistrationButton*/}
+        {/*      className={'ProjectListItem__ButtonPendingClaims'}*/}
+        {/*      text={`${project.pendingClaims.length} Pending Claims`}*/}
+        {/*      ariaLabel={'Pending Claims'}*/}
+        {/*    />*/}
+        {/*    : null*/}
+        {/*  }*/}
+        {/*</div>*/}
+        {/*<div className={'ProjectListItem__Footer'}>*/}
+        {/*  <Grade />*/}
+        {/*  {admin ?*/}
+        {/*    <RegistrationButton*/}
+        {/*      className={'ProjectListItem__ButtonAddTask'}*/}
+        {/*      text={`ADD TASK`}*/}
+        {/*      ariaLabel={'ADD TASK'}*/}
+        {/*    />*/}
+        {/*    : null*/}
+        {/*  }*/}
+        {/*</div>*/}
       </div>
     </m.li>
   )
